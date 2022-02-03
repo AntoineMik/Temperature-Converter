748ef20b92b2e4f348e682bc83d8b0bf37ef5d19
Antoine Vignon <vignonantoinem@gmail.com>
2022-02-03 07:20:56 -0500

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Centigrade and Fahrenheit Temperature Converter</h1>
+    <convert></convert>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Convert from "./components/TemperatureConverter";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Convert
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+/* Global styles */
+.label-wrapper {
+  margin: 0;
+  flex: 0 0 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+[class*="__lg"] {
+  display: inline-block;
+  width: 100%;
+  font-size: 1.9rem;
+}
+[class*="__lg"]:not(:last-child) {
+  margin-bottom: 1rem;
+}
+@media screen and (min-width: 620px) {
+  [class*="__lg"] {
+    font-size: 2.4rem;
+  }
+}
+
+[class*="stack"] > * {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.stack-small > * + * {
+  margin-top: 1.25rem;
+}
+.stack-large > * + * {
+  margin-top: 2.5rem;
+}
+@media screen and (min-width: 550px) {
+  .stack-small > * + * {
+    margin-top: 1.4rem;
+  }
+  .stack-large > * + * {
+    margin-top: 2.8rem;
+  }
+}
+/* End global styles */
+#app {
+  background: #fff;
+  margin: 20rem 0 4rem 0;
+  padding: 1rem;
+  padding-top: 0;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2.5rem 5rem 0 rgba(0, 0, 0, 0.1);
+}
+@media screen and (min-width: 550px) {
+  #app {
+    padding: 4rem;
+  }
+}
+#app > * {
+  max-width: 50rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+#app > form {
+  max-width: 100%;
+}
+#app h1 {
+  display: block;
+  min-width: 100%;
+  width: 100%;
+  text-align: center;
+  margin: 0;
+  margin-bottom: 5rem;
+  margin-top: 1rem;
 }
 </style>
+
---
+++ src/components/TemperatureConverter.vue
@@ -0,0 +1,61 @@
+<template>
+  <form>
+    <h2 class="label-wrapper">
+        <label class="label__lg" for="Temperature in Centigrade">
+            °C
+        </label>
+    </h2>
+    <input
+      type="float"
+      class="input__lg"
+      id="cent-temp"
+      name="cent-temp"
+      autocomplete="off"
+      v-model="centigrade"
+      :placeholder="(fahrenheit - 32) * 5/9"
+      v-on:keyup="clearFahr"
+    />
+
+    <h2 class="label-wrapper">
+        <label class="label__lg" for="Temperature in Fahrenheit">
+            °F
+        </label>
+    </h2>
+    <input
+      type="float"
+      class="input__lg"
+      id="fahr-temp"
+      name="fahr-temp"
+      autocomplete="off"
+      v-model="fahrenheit"
+      :placeholder="(centigrade * 9/5) + 32"
+      v-on:keyup="clearCent"
+    />
+
+  </form>
+  
+</template>
+
+<script>
+    export default {
+
+        data: () => ({
+            centigrade: 20,
+            fahrenheit: '',
+        }),
+
+        methods: {
+
+            clearFahr () {
+                this.fahrenheit = ''
+            },
+
+            clearCent () {
+                this.centigrade = ''
+            },
+        }
+
+    }
+</script>
+
+
